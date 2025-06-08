92983d6269c8f821cf2a7b749cc203dbe178b505
staking4all <staking4all@gmail.com>
2025-06-08 18:15:46 +0000

    Monad Moniotring Public repo

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# monad-monitoring
-Monad Monitoring
+# Monad Monitoring
+
+The purpose of this repo is to assist validators/inidividuals to monitor their own Monad node installation.  
+
+In order to use this repo/guide you need to already have a Monad node running. Also docker must be installed as this monitoring stack makes use of docker.
+
+Currently, the Monad validator sends data to the Monad team using OpenTelemetry, and this process should continue. However, there is no metrics endpoint available from the Monad validator node at this time. This monitoring repository enables you to intercept the OpenTelemetry data, create a metrics endpoint, and still forward the same information to the Monad team. The repository runs its own OpenTelemetry, receives metrics from your Monad node, generates a metrics endpoint, and forwards the data to the Monad team, while also providing a Grafana dashboard for monitoring.
+
+You do not need to use the OpenTelemetry provided with Monad install if using this as it is bundled with the same components. 
+
+## Install 
+
+To make use of this repository and get monitoring running follow the below steps
+
+Clone the repository
+```
+git clone https://github.com/monad-validators/monad-monitoring.git
+```
+
+Check .env variables for Grafana, change as needed
+```
+cd monad-monitoring
+nano .env
+```
+
+To run a Monad node you can either use a binary or docker. To cater for both types of Monad installations we have provided two docker files. `docker-compose-binary.yaml` for a binary based installtion, `docker-compose-docker.yaml` for a docker based installation. Start up the monitoring stack with the relevant file.
+
+For a binary installation use
+```
+docker compose -f docker-compose-binary.yaml up -d
+
+```
+
+For a docker installation use
+```
+docker compose -f docker-compose-docker.yaml up -d
+
+```
+
+Four containers should start that includes
+- OpenTelemetry
+- Prometheus
+- Grafana
+- Node exporter
+
+For docker based Monad installations you must edit Monad Validator nodes `docker-compose.yml` to forward OpenTelemetry traffic to your installation. We assume the Monad validator installation is in your home directory. Within docker-compose.yml replace `--otel-endpoint http://peach10.devcore4.com:4317`  with your local otel `--otel-endpoint http://monad-monitoring-otel-collector-1:4317`. 
+```
+cd /home/monad/
+nano docker-compose.yml
+```
+
+Restart your Monad validator if using docker based version
+```
+cd /home/monad/
+docker compose down
+docker compose up -d
+```
+
+If everything is working correctly you should be able to retrieve metrics on `http://localhost:8889/metrics`
+```
+curl http://localhost:8889/metrics
+```
+
+Open the required ports on your firewall as needed, for example to be able to view the Grafana dashbaord you will need to open port 3000. 
+```
+sudo ufw allow 3000/tcp
+```
+
+You will be able to access a grafana dashboard on `http://<your_own_ip_address>:3000` 
+
+![image](https://github.com/user-attachments/assets/4f22bea3-4752-4fad-8c43-c2f0aee4bc0c)
+
+
+The default dashboard is `Monad monitoring`, an additional dashboard has been added that is `Monad monitoring v2`. v2 adds some extra metrics however needs some extra config.
+
+To use V2 you must make an entry in crontab by adding one of the below line, this script collects extra info to be displayed on the dashbaord
+
+For Monad binary installation
+````
+* * * * * /home/monad/monad-monitoring/textfile-collector/script-data-collector-binary.sh >> /home/monad/error.log
+````
+
+For Monad docker installation
+````
+* * * * * /home/monad/monad-monitoring/textfile-collector/script-data-collector-docker.sh >> /home/monad/error.log
+````
+
+If using a binary installation you need to ensure the user monad for crontab can read the syslog files. 
+````
+sudo usermod -a -G adm monad
+````
+
+v2 adds the following metrics
+- monad specific stats like round, epoch, etc
+- triedb usage stats
+- monad folders that should be cleared and monitored occasionally
+- additional disk metrics
+
+  
+
---
+++ textfile-collector/script-data-collector-binary.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Add Docker environment variables manually
+source /home/monad/.env
+source /home/monad/.bashrc
+source /home/monad/.profile
+
+TARGET_DRIVE=$(grep 'TARGET_DRIVE' /home/monad/.env | cut -d '=' -f2)
+
+# Extract the used and total capacity from local monad_mpt binary
+used_with_unit=$(/usr/local/bin/monad-mpt --storage /dev/$TARGET_DRIVE | grep -A 1 'Capacity' | tail -n 1 | awk '{print $3, $4}' | tr -d '\r')
+capacity_with_unit=$(/usr/local/bin/monad-mpt --storage /dev/$TARGET_DRIVE | grep -A 1 'Capacity' | tail -n 1 | awk '{print $1, $2}' | tr -d '\r')
+
+# Convert total capacity to bytes
+if [[ "${capacity_with_unit,,}" == *"tb"* ]]; then
+    capacity=$(echo "$capacity_with_unit" | sed 's/[Tt][Bb]//')
+    capacity_bytes=$(echo "$capacity * 1024 * 1024 * 1024 * 1024" | bc | awk '{printf "%.0f", $0}')
+elif [[ "${capacity_with_unit,,}" == *"gb"* ]]; then
+    capacity=$(echo "$capacity_with_unit" | sed 's/[Gg][Bb]//')
+    capacity_bytes=$(echo "$capacity * 1024 * 1024 * 1024" | bc | awk '{printf "%.0f", $0}')
+elif [[ "${capacity_with_unit,,}" == *"mb"* ]]; then
+    capacity=$(echo "$capacity_with_unit" | sed 's/[Mm][Bb]//')
+    capacity_bytes=$(echo "$capacity * 1024 * 1024" | bc | awk '{printf "%.0f", $0}')
+elif [[ "${capacity_with_unit,,}" == *"kb"* ]]; then
+    capacity=$(echo "$capacity_with_unit" | sed 's/[Kk][Bb]//')
+    capacity_bytes=$(echo "$capacity * 1024" | bc | awk '{printf "%.0f", $0}')
+else
+    echo "Unexpected capacity unit. Exiting."
+    exit 1
+fi
+
+# Convert used capacity to bytes
+if [[ "${used_with_unit,,}" == *"tb"* ]]; then
+    used=$(echo "$used_with_unit" | sed 's/[Tt][Bb]//')
+    used_bytes=$(echo "$used * 1024 * 1024 * 1024 * 1024" | bc | awk '{printf "%.0f", $0}')
+elif [[ "${used_with_unit,,}" == *"gb"* ]]; then
+    used=$(echo "$used_with_unit" | sed 's/[Gg][Bb]//')
+    used_bytes=$(echo "$used * 1024 * 1024 * 1024" | bc | awk '{printf "%.0f", $0}')
+elif [[ "${used_with_unit,,}" == *"mb"* ]]; then
+    used=$(echo "$used_with_unit" | sed 's/[Mm][Bb]//')
+    used_bytes=$(echo "$used * 1024 * 1024" | bc | awk '{printf "%.0f", $0}')
+elif [[ "${used_with_unit,,}" == *"kb"* ]]; then
+    used=$(echo "$used_with_unit" | sed 's/[Kk][Bb]//')
+    used_bytes=$(echo "$used * 1024" | bc | awk '{printf "%.0f", $0}')
+else
+    echo "Unexpected used unit. Exiting."
+    exit 1
+fi
+
+# Calculate available bytes
+avail_bytes=$(echo "$capacity_bytes - $used_bytes" | bc | awk '{printf "%.0f", $0}')
+
+# Get current epoch from syslog
+current_epoch=$(tail -500 /var/log/syslog | grep -o 'epoch: [0-9]*' | tail -n 1 | awk '{print $2}')
+
+# Get current round from syslog
+current_round=$(tail -500 /var/log/syslog | grep -o 'round: [0-9]*' | tail -n 1 | awk '{print $2}')
+
+# Get forkpoint file count
+forkpoint_dir_count=$(find /home/monad/monad-bft/config/forkpoint -type f | wc -l)
+
+# Get ledger file count
+ledger_dir_count=$(find /home/monad/monad-bft/ledger -type f | wc -l)
+
+# Get wal file count
+wal_dir_count=$(find /home/monad/monad-bft/ -type f -name "wal_*" | wc -l)
+
+# Get the directory where the script is stored
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Set the output file path relative to the script's location
+output_file="$script_dir/data/monad-metrics-data.prom"
+{
+  printf "# HELP mc_triedb_total_bytes Total capacity of /dev/$TARGET_DRIVE\n"
+  printf "# TYPE mc_triedb_total_bytes gauge\n"
+  printf "mc_triedb_total_bytes %s\n" "$capacity_bytes"
+
+  printf "# HELP mc_triedb_used_bytes Used capacity of /dev/$TARGET_DRIVE\n"
+  printf "# TYPE mc_triedb_used_bytes gauge\n"
+  printf "mc_triedb_used_bytes %s\n" "$used_bytes"
+
+  printf "# HELP mc_triedb_avail_bytes Available capacity of /dev/$TARGET_DRIVE\n"
+  printf "# TYPE mc_triedb_avail_bytes gauge\n"
+  printf "mc_triedb_avail_bytes %s\n" "$avail_bytes"
+
+  printf "# HELP mc_current_epoch provides Monad epoch from logs\n"
+  printf "# TYPE mc_current_epoch gauge\n"
+  printf "mc_current_epoch %s\n" "$current_epoch"
+
+  printf "# HELP mc_current_round provides Monad round from logs\n"
+  printf "# TYPE mc_current_round gauge\n"
+  printf "mc_current_round %s\n" "$current_round"
+
+  printf "# HELP mc_forkpoint_dir_count provides file count of Monad forkpoint directory "
+  printf "# TYPE mc_forkpoint_dir_count gauge\n"
+  printf "mc_forkpoint_dir_count %s\n" "$forkpoint_dir_count"
+
+  printf "# HELP mc_ledger_dir_count provides file count of Monad ledger directory "
+  printf "# TYPE mc_ledger_dir_count gauge\n"
+  printf "mc_ledger_dir_count %s\n" "$ledger_dir_count"
+
+  printf "# HELP mc_wal_dir_count provides file count of Monad wal files "
+  printf "# TYPE mc_wal_dir_count gauge\n"
+  printf "mc_wal_dir_count %s\n" "$wal_dir_count"
+
+} > "$output_file"
